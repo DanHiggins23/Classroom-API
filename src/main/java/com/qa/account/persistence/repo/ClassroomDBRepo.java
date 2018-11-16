4de56d84025029a1d8a7dd49d0b30a03456f8cc2
DanHiggins23 <danhiggins23@outlook.com>
2018-11-16 11:38:29 +0000

    Sixth Commit - added jax activator and classroom endpoint

--- src/main/java/com/qa/account/persistence/repo/ClassroomDBRepo.java
+++ src/main/java/com/qa/account/persistence/repo/ClassroomDBRepo.java
@@ -10,6 +10,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
 import static javax.transaction.Transactional.TxType.SUPPORTS;
 
 @Transactional(SUPPORTS)
@@ -24,20 +25,25 @@ public class ClassroomDBRepo implements ClassroomInterface {
     @Override
     public String getAllClassrooms() {
         TypedQuery<Classroom> query = em.createQuery("SELECT c FROM Classroom c  ORDER BY c.classroomID ASC", Classroom.class);
-        return null;
+        return util.getJSONForObject(query.getResultList());
     }
 
     @Override
+    @Transactional(REQUIRED)
     public String createUser(String jsonString) {
-        return null;
+        Classroom newClassroom = util.getObjectForJSON(jsonString, Classroom.class);
+        em.persist(newClassroom);
+        return "{message: account created}";
     }
 
     @Override
+    @Transactional(REQUIRED)
     public String updateUser(String jsonString) {
         return null;
     }
 
     @Override
+    @Transactional(REQUIRED)
     public String deleteUser(String jsonString) {
         return null;
     }
